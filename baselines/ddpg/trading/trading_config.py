97f90d160803c8ad8d9294c0b2aca79bdaa0a453
philipshurpik <philip.shurpik@gmail.com>
2018-03-26 19:39:12 +0300

    updated trading config

--- baselines/ddpg/trading/trading_config.py
+++ baselines/ddpg/trading/trading_config.py
@@ -2,13 +2,22 @@ from box import Box
 
 config = Box({
     "data": {
-        "file_name": "data/BTCETH60.csv",
+        "file_name": "data/USDBTC60.csv",
         "episode_duration": 480,
-        "amplitude": None
+        "date_columns": ["Date", "Time"],
+        "index_column": "Date_Time",
+        "start_date": None,
+        "train_split": 0.8,
+        "initial_cash": 1000,
+        "commission": 0.0025,
+        "amplitude": None,
     },
     "seed": 42,
     "model": {
+        # type - conv | fc | lstm
+        "type": "fc",
         "stocks_number": 1,
-        "window_size": 30
-    }
+        "window_size": 120,
+        "save_folder": None
+    },
 })
